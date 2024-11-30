e8b94da9b735610212a55df0cf1c24960b6e0f2e
VanyaVanchez <145760977+VanyaVanchez@users.noreply.github.com>
2024-11-30 14:03:34 +0300

    Update document.h

--- search-server/document.h
+++ search-server/document.h
@@ -12,7 +12,7 @@ enum class DocumentStatus {
 struct Document {
     Document() = default;
 
-    Document(int id, double relevance, int rating);
+    Document(int id, double relevance, int rating); 
 
     int id = 0;
     double relevance = 0.0;
